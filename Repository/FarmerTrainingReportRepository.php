29e6e0d90f80f943f7a9dc2e75a34872f9cda25c
sohel_rbs <sohel@rightbrainsolution.com>
2021-02-09 10:40:39 +0600

    Added farmer training report for poultry

--- Repository/FarmerTrainingReportRepository.php
+++ Repository/FarmerTrainingReportRepository.php
@@ -21,8 +21,40 @@ use Doctrine\ORM\EntityRepository;
  *
  * @author Md Shafiqul islam <shafiqabs@gmail.com>
  */
-class FarmerTrainingReportRepository extends EntityRepository
+class FarmerTrainingReportRepository extends BaseRepository
 {
+    public function getFarmerTrainingReport($filterBy)
+    {
+//        dd($filterBy);
+        $qb = $this->createQueryBuilder('e');
+        $qb->select('e.trainingTopics','e.trainingDate', 'e.remarks', 'e.trainingMaterial');
+        $qb->addSelect('agent.name AS agentName','agent.address AS agentAddress', 'agent.mobile AS agentMobile');
+        $qb->addSelect('farmer.name AS farmerName', 'farmer.address AS farmerAddress', 'farmer.mobile AS farmerMobile');
+        $qb->addSelect('farmerTrainingReportDetails.farmerCapacity','farmerTrainingReportDetails.trainingMaterialQty');
 
+        $qb->where('breedName.slug = :breedTypeSlug')->setParameter('breedTypeSlug', $filterBy['breedTypeSlug']);
+        $qb->andWhere('e.trainingDate >= :bOfYear')->setParameter('bOfYear', $filterBy['bOfYear']);
+        $qb->andWhere('e.trainingDate <= :eOfYear')->setParameter('eOfYear', $filterBy['eOfYear']);
+
+        $qb->leftJoin('e.agent', 'agent');
+        $qb->leftJoin('e.farmerTrainingReportDetails','farmerTrainingReportDetails');
+        $qb->leftJoin('farmerTrainingReportDetails.customer','farmer');
+        $qb->leftJoin('e.breedName', 'breedName');
+//        $qb->leftJoin('e.employee', 'employee');
+        $this->handleSearchFilterBetween($qb, $filterBy);
+
+
+        /** @var TYPE_NAME $results */
+        $results = $qb->getQuery()->getArrayResult();
+        $data = [];
+        foreach ($results as $result) {
+            $month = $result['trainingDate']->format('F-Y');
+
+            $data[$month][] = $result;
+        }
+
+//        dd($data);
+        return $data;
+    }
 
 }
