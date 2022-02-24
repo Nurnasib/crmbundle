b760a9fb79ffbcc3f619155843f3be57adf4ebcc
sohel_rbs <sohel@rightbrainsolution.com>
2022-02-24 12:39:31 +0600

    update expense report

--- Repository/ExpenseRepository.php
+++ Repository/ExpenseRepository.php
@@ -25,7 +25,7 @@ use function Doctrine\ORM\QueryBuilder;
  */
 class ExpenseRepository extends EntityRepository
 {
-    public function getExpense($filterBy, User $user)
+    public function getExpense($filterBy, User $loggedUser)
     {
         $start = isset($filterBy['startDate']) ? (new \DateTime($filterBy['startDate']))->format('Y-m-d 00:00:00') : null;
         $end = isset($filterBy['endDate']) ? (new \DateTime($filterBy['endDate']))->format('Y-m-d 23:59:59') : null;
@@ -35,44 +35,65 @@ class ExpenseRepository extends EntityRepository
         $qb = $this->createQueryBuilder('e');
         $qb->join('e.crmVisit', 'crm_visit');
         $qb->leftJoin('e.visitingArea', 'visiting_area');
+        $qb->leftJoin('e.purpose', 'purpose');
+        $qb->leftJoin('e.vehicle', 'vehicle');
         $qb->join('crm_visit.employee', 'employee');
         $qb->leftJoin('employee.designation', 'designation');
+        $qb->leftJoin('employee.lineManager', 'lineManager');
 
         $qb->select('e AS details');
-        $qb->addSelect('crm_visit.created AS visitedDate', 'visiting_area.name AS visitingAreaName');
+        $qb->addSelect('crm_visit.id AS visitId','crm_visit.created AS visitedDate', 'visiting_area.name AS visitingAreaName');
         $qb->addSelect('employee.userId', 'employee.name', 'designation.name AS designationName');
+        $qb->addSelect('purpose.id AS purposeId','purpose.name AS purposeName');
+        $qb->addSelect('vehicle.id AS vehicleId','vehicle.name AS vehicleName');
 
+//        $qb->groupBy('purpose.id');
+//        $qb->addGroupBy('vehicle.id');
+//        $qb->addGroupBy('crm_visit.id');
+        $rolesString = implode($loggedUser->getRoles(), '_');
 
-        if (isset($filterBy['employee']) && $filterBy['employee'] !== null){
-            $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $employeeId);
-        }else{
-            $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $user->getId());
-
+        if (!str_contains($rolesString, 'ADMIN') && !in_array('ROLE_LINE_MANAGER', $loggedUser->getRoles())){
+            $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $loggedUser->getId());
+        }elseif (in_array('ROLE_LINE_MANAGER', $loggedUser->getRoles())){
+            if (isset($filterBy['employee']) && $filterBy['employee'] !== null){
+                $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $employeeId);
+            }else{
+                $qb->andWhere('lineManager.id = :lineManagerId')->setParameter('lineManagerId', $loggedUser->getId());
+            }
         }
 
         $qb->andWhere('crm_visit.created >= :start')->setParameter('start', $start);
         $qb->andWhere('crm_visit.created <= :end')->setParameter('end', $end);
+//        $qb->andWhere('crm_visit.id = 196');
 
         $results = $qb->getQuery()->getArrayResult();
 
         $data = [];
 
         foreach ($results as $key => $result) {
-            $month = $result['visitedDate']->format('m-F');
 
+            $yearMonth = $result['visitedDate']->format('Y-m-F');
+
+
+            $result['details']['visitId'] = $result['visitId'];
             $result['details']['visitedDate'] = $result['visitedDate'];
             $result['details']['visitingAreaName'] = $result['visitingAreaName'];
 
-            $data[$result['visitedDate']->format('Y')][$month]['details'][] = $result['details'];
-            $data[$result['visitedDate']->format('Y')][$month]['employee'] = [
+            $data[$yearMonth][$result['userId']]['details'][$result['visitId']] = $result['details'];
+            $data[$yearMonth][$result['userId']]['details'][$result['visitId']]['purpose'][] = $result['purposeName'];
+//            $data[$yearMonth][$result['userId']]['details'][$result['visitId']]['purpose'][] = $result['purposeName'];
+//            $data[$yearMonth][$result['userId']]['details'][$result['visitId']]['vehicle'][] = $result['vehicleName'];
+
+            $data[$yearMonth][$result['userId']]['employee'] = [
                 'userId' => $result['userId'],
                 'name' => $result['name'],
                 'designation' => $result['designationName'],
             ];
 
             ksort($data);
-            ksort($data[$result['visitedDate']->format('Y')]);
         }
+
+        dd($results);
         return $data;
     }
 
