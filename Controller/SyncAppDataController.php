943b99d7dbdb77d04422a1f087f931e2e8ea49ce
rakeybulhasan <rakeybulhasan@gmail.com>
2022-03-29 12:47:08 +0600

    lab service modify

--- Controller/SyncAppDataController.php
+++ Controller/SyncAppDataController.php
@@ -1412,18 +1412,18 @@ VALUES (:schedule_visit, :conveyance, :daily_allowance, :hotel_rent, :photostate
 
                 $stmt->bindValue('id', $exitingLabService['id']);
 
-                $stmt->bindValue('january', $report['january'] && $report['january']!="" ?: $exitingLabService['january']);
-                $stmt->bindValue('february',$report['february'] && $report['february']!="" ?: $exitingLabService['february']);
-                $stmt->bindValue('march', $report['march'] && $report['march']!="" ?: $exitingLabService['march']);
-                $stmt->bindValue('april', $report['april'] && $report['april']!="" ?: $exitingLabService['april']);
-                $stmt->bindValue('may', $report['may'] && $report['may']!="" ?: $exitingLabService['may']);
-                $stmt->bindValue('june', $report['june'] && $report['june']!="" ?: $exitingLabService['june']);
-                $stmt->bindValue('july', $report['july'] && $report['july']!="" ?: $exitingLabService['july']);
-                $stmt->bindValue('august', $report['august']&&$report['august']!="" ?: $exitingLabService['august']);
-                $stmt->bindValue('september', $report['september']&&$report['september']!="" ?: $exitingLabService['september']);
-                $stmt->bindValue('october', $report['october'] && $report['october']!="" ?: $exitingLabService['october']);
-                $stmt->bindValue('november', $report['november'] && $report['november']!="" ?: $exitingLabService['november']);
-                $stmt->bindValue('december', $report['december']&&$report['december']!="" ?: $exitingLabService['december']);
+                $stmt->bindValue('january', $report['january'] && $report['january']!=""?$report['january']: $exitingLabService['january']);
+                $stmt->bindValue('february',$report['february'] && $report['february']!=""?$report['february']: $exitingLabService['february']);
+                $stmt->bindValue('march', $report['march'] && $report['march']!=""?$report['march']: $exitingLabService['march']);
+                $stmt->bindValue('april', $report['april'] && $report['april']!=""?$report['april']: $exitingLabService['april']);
+                $stmt->bindValue('may', $report['may'] && $report['may']!=""?$report['may']: $exitingLabService['may']);
+                $stmt->bindValue('june', $report['june'] && $report['june']!=""?$report['june']: $exitingLabService['june']);
+                $stmt->bindValue('july', $report['july'] && $report['july']!=""?$report['july']: $exitingLabService['july']);
+                $stmt->bindValue('august', $report['august']&&$report['august']!=""?$report['august']: $exitingLabService['august']);
+                $stmt->bindValue('september', $report['september']&&$report['september']!=""?$report['september']: $exitingLabService['september']);
+                $stmt->bindValue('october', $report['october'] && $report['october']!=""?$report['october']: $exitingLabService['october']);
+                $stmt->bindValue('november', $report['november'] && $report['november']!=""?$report['november']: $exitingLabService['november']);
+                $stmt->bindValue('december', $report['december']&&$report['december']!=""?$report['december']: $exitingLabService['december']);
                 $stmt->bindValue('created_at', $createdAt);
                 $stmt->execute();
             }else{
