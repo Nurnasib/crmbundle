819f3783880f2f96c1c48961bb010093098dc44b
rakeybulhasan <rakeybulhasan@gmail.com>
2022-07-21 12:09:02 +0600

    chick and layer life cycle report done.

--- Repository/ChickLifeCycleDetailsRepository.php
+++ Repository/ChickLifeCycleDetailsRepository.php
@@ -26,68 +26,137 @@ class ChickLifeCycleDetailsRepository extends BaseRepository
 {
     public function getChickLifeCycleDetails($lifeCycleSlug, $filterBy)
     {
-        $startDate = isset($filterBy['startDate']) ? (new \DateTime($filterBy['startDate']))->format('Y-m-d') . ' 00:00:00' : null;
-        $endDate = isset($filterBy['endDate']) ? (new \DateTime($filterBy['endDate']))->format('Y-m-d') . ' 23:59:59' : null;
-
-        $qb = $this->createQueryBuilder('e');
-        $qb->join('e.crmChickLifeCycle', 'crm_chick_life_cycle');
-        $qb->join('crm_chick_life_cycle.report', 'report');
-        $qb->join('crm_chick_life_cycle.agent', 'agent');
-        $qb->join('crm_chick_life_cycle.customer', 'customer');
-        $qb->leftJoin('crm_chick_life_cycle.hatchery', 'hatchery');
-        $qb->leftJoin('crm_chick_life_cycle.feed', 'feed');
-        $qb->leftJoin('crm_chick_life_cycle.breed', 'breed');
-        $qb->leftJoin('crm_chick_life_cycle.employee', 'employee');
-        $qb->leftJoin('e.feedType', 'feed_type');
-
-        $qb->select('e AS details');
-        $qb->addSelect('agent.name AS agentName','agent.address AS agentAddress');
-        $qb->addSelect('customer.name AS customerName','customer.address AS customerAddress','customer.mobile AS customerMobile');
-        $qb->addSelect('crm_chick_life_cycle.id AS lifeCycleId','crm_chick_life_cycle.lifeCycleState', 'crm_chick_life_cycle.totalBirds', 'crm_chick_life_cycle.hatchingDate');
-        $qb->addSelect('hatchery.name AS hatcheryName');
-        $qb->addSelect('feed.name AS feedName');
-        $qb->addSelect('breed.name AS breedName');
-        $qb->addSelect('feed_type.name AS feedTypeName');
-
-        $qb->where('report.slug = :slug')->setParameter('slug', $lifeCycleSlug);
-        $qb->andWhere('e.reportingDate >= :startDate')->setParameter('startDate', $startDate);
-        $qb->andWhere('e.reportingDate <= :endDate')->setParameter('endDate', $endDate);
-        $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $filterBy['employeeId']);
-        $qb->andWhere('crm_chick_life_cycle.lifeCycleState = :reportStatus')->setParameter('reportStatus', $filterBy['reportStatus']);
-        if ($filterBy['farmerId']){
-            $qb->andWhere('customer.id = :farmerId')->setParameter('farmerId', $filterBy['farmerId']);
-        }
-
-        $results =  $qb->getQuery()->getArrayResult();
         $data = [];
+        $lifeCycleIdsMonthWise=$this->getChickLifeCycles($lifeCycleSlug, $filterBy);
+//        dd($lifeCycleIdsMonthWise);
+        if(sizeof($lifeCycleIdsMonthWise)>0){
+            foreach ($lifeCycleIdsMonthWise as $monthYear=>$lifeCycleIds) {
+                foreach ($lifeCycleIds as $lifeCycleId=>$item){
+//                    dd($item);
+                    $qb = $this->createQueryBuilder('e');
+                    $qb->join('e.crmChickLifeCycle', 'crm_chick_life_cycle');
+                    $qb->join('crm_chick_life_cycle.report', 'report');
+                    $qb->join('crm_chick_life_cycle.agent', 'agent');
+                    $qb->join('crm_chick_life_cycle.customer', 'customer');
+                    $qb->leftJoin('crm_chick_life_cycle.hatchery', 'hatchery');
+                    $qb->leftJoin('crm_chick_life_cycle.feed', 'feed');
+                    $qb->leftJoin('crm_chick_life_cycle.breed', 'breed');
+                    $qb->leftJoin('crm_chick_life_cycle.employee', 'employee');
+                    $qb->leftJoin('e.feedType', 'feed_type');
 
-        foreach ($results as $result) {
-            $month = ($result['details']['reportingDate'])->format('m-F-Y');
+                    $qb->select('e AS details');
+                    $qb->addSelect('agent.name AS agentName','agent.address AS agentAddress');
+                    $qb->addSelect('customer.id AS customerId','customer.name AS customerName','customer.address AS customerAddress','customer.mobile AS customerMobile');
+                    $qb->addSelect('crm_chick_life_cycle.id AS lifeCycleId','crm_chick_life_cycle.lifeCycleState', 'crm_chick_life_cycle.totalBirds', 'crm_chick_life_cycle.hatchingDate');
+                    $qb->addSelect('hatchery.name AS hatcheryName');
+                    $qb->addSelect('feed.name AS feedName');
+                    $qb->addSelect('breed.name AS breedName');
+                    $qb->addSelect('feed_type.name AS feedTypeName');
 
-            $result['details']['feedTypeName'] = $result['feedTypeName'];
+                    $qb->where('report.slug = :slug')->setParameter('slug', $lifeCycleSlug);
+//            $qb->andWhere('e.reportingDate >= :startDate')->setParameter('startDate', $startDate);
+                    $qb->andWhere('e.visitingWeek <= :visitingWeek')->setParameter('visitingWeek', (int)$item['maxWeek']);
+                    $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $filterBy['employeeId']);
+                    $qb->andWhere('crm_chick_life_cycle.lifeCycleState = :reportStatus')->setParameter('reportStatus', $filterBy['reportStatus']);
+                    if ($filterBy['farmerId']){
+                        $qb->andWhere('customer.id = :farmerId')->setParameter('farmerId', $filterBy['farmerId']);
+                    }
+                    $qb->andWhere('crm_chick_life_cycle.id = :lifeCycleId')->setParameter('lifeCycleId', $lifeCycleId);
 
-            $data[$month][$result['lifeCycleId']]['details'][] = $result['details'];
-            $data[$month][$result['lifeCycleId']]['parent'] = [
-                'customerName' => $result['customerName'],
-                'customerAddress' => $result['customerAddress'],
-                'customerMobile' => $result['customerMobile'],
-                'agentName' => $result['agentName'],
-                'agentAddress' => $result['agentAddress'],
-                'lifeCycleId' => $result['lifeCycleId'],
-                'lifeCycleState' => $result['lifeCycleState'],
-                'totalBirds' => $result['totalBirds'],
-                'hatchingDate' => $result['hatchingDate'],
-                'hatcheryName' => $result['hatcheryName'],
-                'feedName' => $result['feedName'],
-                'breedName' => $result['breedName'],
-            ];
+                    $results =  $qb->getQuery()->getArrayResult();
+                    foreach ($results as $result) {
+                        $result['details']['feedTypeName'] = $result['feedTypeName'];
+                        $data[$monthYear][$lifeCycleId]['details'][] = $result['details'];
+                        $data[$monthYear][$lifeCycleId]['parent'] = [
+                            'customerName' => $result['customerName'],
+                            'customerAddress' => $result['customerAddress'],
+                            'customerMobile' => $result['customerMobile'],
+                            'agentName' => $result['agentName'],
+                            'agentAddress' => $result['agentAddress'],
+                            'lifeCycleId' => $result['lifeCycleId'],
+                            'lifeCycleState' => $result['lifeCycleState'],
+                            'totalBirds' => $result['totalBirds'],
+                            'hatchingDate' => $result['hatchingDate'],
+                            'hatcheryName' => $result['hatcheryName'],
+                            'feedName' => $result['feedName'],
+                            'breedName' => $result['breedName'],
+                        ];
+                    }
+//                    ksort($data);
+                }
+
+            }
+
         }
-        ksort($data);
+
         return $data;
     }
 
+    private function getChickLifeCycles($lifeCycleSlug, $filterBy){
+        $startDate = isset($filterBy['startDate']) ? (new \DateTime($filterBy['startDate']))->format('Y-m-d') . ' 00:00:00' : null;
+        $endDate = isset($filterBy['endDate']) ? (new \DateTime($filterBy['endDate']))->format('Y-m-d') . ' 23:59:59' : null;
 
+        $months = $this->getMonthRanges($startDate, $endDate);
+        $returnArray=[];
+        if(sizeof($months)>0){
+            foreach ($months as $month){
+                $fromDate= $month['start'];
+                $toDate= $month['end'];
+                $qb = $this->createQueryBuilder('e');
+                $qb->join('e.crmChickLifeCycle', 'crm_chick_life_cycle');
+                $qb->join('crm_chick_life_cycle.report', 'report');
+                $qb->join('crm_chick_life_cycle.customer', 'customer');
+                $qb->leftJoin('crm_chick_life_cycle.employee', 'employee');
+                $qb->select('crm_chick_life_cycle.id');
+                $qb->addSelect('MAX(e.visitingWeek) as maxWeek');
+                $qb->where('report.slug = :slug')->setParameter('slug', $lifeCycleSlug);
+                $qb->andWhere('e.reportingDate >= :startDate')->setParameter('startDate', $fromDate);
+                $qb->andWhere('e.reportingDate <= :endDate')->setParameter('endDate', $toDate);
+                $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $filterBy['employeeId']);
+                $qb->andWhere('crm_chick_life_cycle.lifeCycleState = :reportStatus')->setParameter('reportStatus', $filterBy['reportStatus']);
+                if ($filterBy['farmerId']){
+                    $qb->andWhere('customer.id = :farmerId')->setParameter('farmerId', $filterBy['farmerId']);
+                }
+                $qb->groupBy('crm_chick_life_cycle.id');
 
+                $results =  $qb->getQuery()->getArrayResult();
+
+                $monthYear= date('F-Y', strtotime($toDate));
+
+                if($results){
+                    foreach ($results as $result) {
+                        $returnArray[$monthYear][$result['id']]=$result;
+                    }
+                }
+            }
+        }
+
+        return $returnArray;
+    }
+
+   private function getMonthRanges($start, $end) {
+        $timeStart = strtotime($start);
+        $timeEnd   = strtotime($end);
+        $out       = [];
+
+        $milestones[] = $timeStart;
+        $timeEndMonth = strtotime('first day of next month midnight', $timeStart);
+        while ($timeEndMonth < $timeEnd) {
+            $milestones[] = $timeEndMonth;
+            $timeEndMonth = strtotime('+1 month', $timeEndMonth);
+        }
+        $milestones[] = $timeEnd;
+
+        $count = count($milestones);
+        for ($i = 1; $i < $count; $i++) {
+            $out[] = [
+                'start' => date('Y-m-d H:i:s', $milestones[$i-1]), // Here you can apply your formatting (like "date('Y-m-d H:i:s', $milestones[$i-1])") if you don't won't want just timestamp
+                'end'   => date('Y-m-d H:i:s', ($milestones[$i] - 1))
+            ];
+        }
+
+        return $out;
+    }
 
     public function getNumberOfReportsForKpi($board)
     {
--- Repository/LayerLifeCycleDetailsRepository.php
+++ Repository/LayerLifeCycleDetailsRepository.php
@@ -26,15 +26,89 @@ class LayerLifeCycleDetailsRepository extends BaseRepository
 {
     public function getLayerLifeCycleDetails($lifeCycleSlug, $filterBy)
     {
-//        $startDate = $filterBy['startDate'] ? (new \DateTime($filterBy['startDate']))->format('Y-m-d') : '';
-//        $endDate = $filterBy['endDate'] ? (new \DateTime($filterBy['endDate']))->format('Y-m-d') : '';
+        $startDate = $filterBy['startDate'] ? (new \DateTime($filterBy['startDate']))->format('Y-m-d') : '';
+        $endDate = $filterBy['endDate'] ? (new \DateTime($filterBy['endDate']))->format('Y-m-d') : '';
+
+        $data = [];
+
+        $lifeCycleIds=$this->getLayerLifeCycles($lifeCycleSlug, $filterBy);
+
+        if(sizeof($lifeCycleIds)){
+            foreach ($lifeCycleIds as $lifeCycleId=>$item) {
+                $qb = $this->createQueryBuilder('e');
+
+                $qb->join('e.crmLayerLifeCycle', 'crm_layer_life_cycle');
+                $qb->join('crm_layer_life_cycle.report', 'report');
+                $qb->join('crm_layer_life_cycle.employee', 'employee');
+//        $qb->leftJoin('crm_layer_life_cycle.agent', 'agent');
+                $qb->join('crm_layer_life_cycle.customer', 'customer');
+                $qb->leftJoin('crm_layer_life_cycle.hatchery', 'hatchery');
+                $qb->leftJoin('crm_layer_life_cycle.feed', 'feed');
+                $qb->leftJoin('crm_layer_life_cycle.breed', 'breed');
+                $qb->leftJoin('e.feedType', 'feed_type');
+                $qb->leftJoin('e.feedMill', 'feed_mill');
+
+                $qb->select('e AS details');
+//        $qb->addSelect('agent.name AS agentName','agent.address AS agentAddress');
+                $qb->addSelect('customer.name AS customerName','customer.address AS customerAddress','customer.mobile AS customerMobile');
+                $qb->addSelect('crm_layer_life_cycle.id AS lifeCycleId','crm_layer_life_cycle.lifeCycleState', 'crm_layer_life_cycle.totalBirds', 'crm_layer_life_cycle.hatcheryDate');
+                $qb->addSelect('hatchery.name AS hatcheryName');
+                $qb->addSelect('feed.name AS feedName');
+                $qb->addSelect('breed.name AS breedName');
+                $qb->addSelect('feed_type.name AS feedTypeName');
+                $qb->addSelect('feed_mill.name AS feedMillName');
+
+                $qb->where('report.slug = :slug')->setParameter('slug', $lifeCycleSlug);
+//                $qb->andWhere('e.visitingDate >= :startDate')->setParameter('startDate', $startDate);
+                $qb->andWhere('e.ageWeek <= :maxWeek')->setParameter('maxWeek', (int)$item['maxWeek']);
+                $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $filterBy['employeeId']);
+                $qb->andWhere('crm_layer_life_cycle.id = :id')->setParameter('id', $lifeCycleId);
+
+                /*if(isset($filterBy['farmerId']) && $filterBy['farmerId']!=""){
+                    $qb->andWhere('customer.id = :farmerId')->setParameter('farmerId', $filterBy['farmerId']);
+                }*/
+
+
+                $results = $qb->getQuery()->getArrayResult();
+
+                foreach ($results as $result) {
+
+                    $result['details']['feedTypeName'] = $result['feedTypeName'];
+                    $result['details']['feedMillName'] = $result['feedMillName'];
+
+                    $data[$result['lifeCycleId']]['details'][] = $result['details'];
+                    $data[$result['lifeCycleId']]['parent'] = [
+                        'customerName' => $result['customerName'],
+                        'customerAddress' => $result['customerAddress'],
+                        'customerMobile' => $result['customerMobile'],
+                        'lifeCycleId' => $result['lifeCycleId'],
+                        'lifeCycleState' => $result['lifeCycleState'],
+                        'totalBirds' => $result['totalBirds'],
+                        'hatchingDate' => $result['hatcheryDate'],
+                        'hatcheryName' => $result['hatcheryName'],
+                        'feedName' => $result['feedName'],
+                        'breedName' => $result['breedName'],
+                    ];
+                }
+            }
+        }
+
+
+        ksort($data);
+//        dd($data);
+        return $data;
+
+    }
+
+    private function getLayerLifeCycles($lifeCycleSlug, $filterBy){
+        $startDate = $filterBy['startDate'] ? (new \DateTime($filterBy['startDate']))->format('Y-m-d') : '';
+        $endDate = $filterBy['endDate'] ? (new \DateTime($filterBy['endDate']))->format('Y-m-d') : '';
 
         $qb = $this->createQueryBuilder('e');
 
         $qb->join('e.crmLayerLifeCycle', 'crm_layer_life_cycle');
         $qb->join('crm_layer_life_cycle.report', 'report');
         $qb->join('crm_layer_life_cycle.employee', 'employee');
-//        $qb->leftJoin('crm_layer_life_cycle.agent', 'agent');
         $qb->join('crm_layer_life_cycle.customer', 'customer');
         $qb->leftJoin('crm_layer_life_cycle.hatchery', 'hatchery');
         $qb->leftJoin('crm_layer_life_cycle.feed', 'feed');
@@ -42,55 +116,33 @@ class LayerLifeCycleDetailsRepository extends BaseRepository
         $qb->leftJoin('e.feedType', 'feed_type');
         $qb->leftJoin('e.feedMill', 'feed_mill');
 
-        $qb->select('e AS details');
-//        $qb->addSelect('agent.name AS agentName','agent.address AS agentAddress');
-        $qb->addSelect('customer.name AS customerName','customer.address AS customerAddress','customer.mobile AS customerMobile');
-        $qb->addSelect('crm_layer_life_cycle.id AS lifeCycleId','crm_layer_life_cycle.lifeCycleState', 'crm_layer_life_cycle.totalBirds', 'crm_layer_life_cycle.hatcheryDate');
-        $qb->addSelect('hatchery.name AS hatcheryName');
-        $qb->addSelect('feed.name AS feedName');
-        $qb->addSelect('breed.name AS breedName');
-        $qb->addSelect('feed_type.name AS feedTypeName');
-        $qb->addSelect('feed_mill.name AS feedMillName');
+        $qb->select('MAX(e.ageWeek) AS maxWeek');
+        $qb->addSelect('crm_layer_life_cycle.id AS lifeCycleId');
 
         $qb->where('report.slug = :slug')->setParameter('slug', $lifeCycleSlug);
-//        $qb->andWhere('e.visitingDate >= :startDate')->setParameter('startDate', $startDate);
-//        $qb->andWhere('e.visitingDate <= :endDate')->setParameter('endDate', $endDate);
-        $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $filterBy['employeeId']);
-        $qb->andWhere('crm_layer_life_cycle.lifeCycleState = :reportStatus')->setParameter('reportStatus', $filterBy['reportStatus']);
-        $qb->andWhere('customer.id = :farmerId')->setParameter('farmerId', $filterBy['farmerId']);
-
-
-        $results = $qb->getQuery()->getArrayResult();
-
-        $data = [];
-
-        foreach ($results as $result) {
-//            $month = ($result['details']['visitingDate'])->format('m-F-Y');
-
-            $result['details']['feedTypeName'] = $result['feedTypeName'];
-            $result['details']['feedMillName'] = $result['feedMillName'];
-
-            $data[$result['lifeCycleId']]['details'][] = $result['details'];
-            $data[$result['lifeCycleId']]['parent'] = [
-                'customerName' => $result['customerName'],
-                'customerAddress' => $result['customerAddress'],
-                'customerMobile' => $result['customerMobile'],
-//                'agentName' => $result['agentName'],
-//                'agentAddress' => $result['agentAddress'],
-                'lifeCycleId' => $result['lifeCycleId'],
-                'lifeCycleState' => $result['lifeCycleState'],
-                'totalBirds' => $result['totalBirds'],
-                'hatchingDate' => $result['hatcheryDate'],
-                'hatcheryName' => $result['hatcheryName'],
-                'feedName' => $result['feedName'],
-                'breedName' => $result['breedName'],
-            ];
+        if($startDate && $endDate){
+            $qb->andWhere('e.visitingDate >= :startDate')->setParameter('startDate', $startDate);
+            $qb->andWhere('e.visitingDate <= :endDate')->setParameter('endDate', $endDate);
         }
 
-        ksort($data);
-//        dd($data);
-        return $data;
+        $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $filterBy['employeeId']);
+        $qb->andWhere('crm_layer_life_cycle.lifeCycleState = :reportStatus')->setParameter('reportStatus', $filterBy['reportStatus']);
 
+        if(isset($filterBy['farmerId']) && $filterBy['farmerId']!=""){
+            $qb->andWhere('customer.id = :farmerId')->setParameter('farmerId', $filterBy['farmerId']);
+        }
+        $qb->groupBy('crm_layer_life_cycle.id');
+
+        $results = $qb->getQuery()->getArrayResult();
+//        dd($results);
+        $returnArray=[];
+        if($results){
+            foreach ($results as $result) {
+                $returnArray[$result['lifeCycleId']]=$result;
+            }
+        }
+//dd($returnArray);
+        return $returnArray;
     }
 
     public function getNumberOfReportsForKpi($board)
