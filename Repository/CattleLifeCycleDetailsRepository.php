2d6113f21af3b2a5790277e1aed071cd3fb5ef62
rakeybulhasan <rakeybulhasan@gmail.com>
2023-03-30 14:13:01 +0600

    other report rearrange done

--- Repository/CattleLifeCycleDetailsRepository.php
+++ Repository/CattleLifeCycleDetailsRepository.php
@@ -46,7 +46,7 @@ class CattleLifeCycleDetailsRepository extends EntityRepository
         $qb->andWhere('report.slug = :reportSlug')->setParameter('reportSlug', $lifeCycleSlug);
         $qb->andWhere('crm_cattle_life_cycle.lifeCycleState = :reportStatus')->setParameter('reportStatus', $filterBy['reportStatus']);
         $qb->andWhere('employee.id = :employeeId')->setParameter('employeeId', $filterBy['employeeId']);
-        if ($filterBy['farmerId']){
+        if (isset($filterBy['farmerId']) && $filterBy['farmerId']!=''){
             $qb->andWhere('customer.id = :farmerId')->setParameter('farmerId', $filterBy['farmerId']);
         }
 
