03e0e20620bb2f83608a9917a95eab0b874464ba
Rakeybul Hasan <rakeybulhasan@rightbrainsolution.com>
2023-07-26 16:23:46 +0600

    monthly report modify

--- Repository/ChallengerRepository.php
+++ Repository/ChallengerRepository.php
@@ -94,4 +94,60 @@ class ChallengerRepository extends EntityRepository
 //        dd($returnArray);
         return $returnArray;
     }
+
+    public function getChallengerByEmployeeForKpiMonthlyReport($report, $filterBy)
+    {
+        $returnArray=[];
+//dd($filterBy);
+        if(!empty($report) && $filterBy['employee'] !=""){
+            $qb = $this->createQueryBuilder('e');
+
+            $qb->select('e.id', 'e.createdAt','e.challengerType','e.name','e.description', 'e.problemOn');
+
+            $qb->addSelect('employee.id AS employeeId');
+            $qb->addSelect('employee.name AS employeeName');
+            $qb->addSelect('designation.name AS designationName');
+            $qb->addSelect('challengerFeed.name AS feedName');
+
+            $qb->join('e.employee','employee');
+            $qb->join('employee.designation','designation');
+            $qb->leftJoin('e.challengerFeedName','challengerFeed');
+
+            if($report=='challenges-problem'){
+                $qb->where('e.challengerType =:challengerType')->setParameter('challengerType','Problems');
+            }elseif ($report=='challenges-idea'){
+                $qb->where('e.challengerType =:challengerType')->setParameter('challengerType','Idea');
+            }elseif ($report=='competitors-activity'){
+                $qb->where('e.challengerType =:challengerType')->setParameter('challengerType','Competitor Activity');
+            }
+
+            $qb->andWhere('employee = :employee')->setParameter('employee', $filterBy['employee']);
+
+            $year = isset( $filterBy['year']) &&  $filterBy['year']!='' ?  $filterBy['year'] : date('Y');
+            $startMonth = date('Y-m-d', strtotime($year . '-' . $filterBy['startMonth'] . '-01'));
+            $endMonth = date('Y-m-t', strtotime($year . '-' . $filterBy['endMonth'] . '-01'));
+
+            $startDate = isset($filterBy['startMonth']) ? (new \DateTime($startMonth))->format('Y-m-d 00:00:00') : date('Y-m-d 00:00:00');
+            $endDate = isset($filterBy['endMonth']) ? (new \DateTime($endMonth))->format('Y-m-d 23:59:59') : date('Y-m-t 23:59:59');
+
+            $qb->andWhere('e.createdAt >= :reportingMonthStart')->setParameter('reportingMonthStart', $startDate);
+            $qb->andWhere('e.createdAt <= :reportingMonthEnd')->setParameter('reportingMonthEnd', $endDate);
+
+            $results = $qb->getQuery()->getArrayResult();
+            if($results){
+                foreach ($results as $result){
+                    $reportingMonth = $result['createdAt']->format('F-Y');
+                    if($report=='challenges-problem'){
+                        $returnArray[$reportingMonth][$result['feedName']][]=$result;
+                    }elseif ($report=='challenges-idea'){
+                        $returnArray[$reportingMonth][]=$result;
+                    }elseif ($report=='competitors-activity'){
+                        $returnArray[$reportingMonth][]=$result;
+                    }
+
+                }
+            }
+        }
+        return $returnArray;
+    }
 }
