60005a8464b2aa605870470725852ff5a629f135
Rakeybul Hasan <rakeybul.rbs@gmail.com>
2023-12-30 11:37:35 +0600

    api controller modify

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -338,7 +338,7 @@ class ApiController extends AbstractController
             $crm_expense_chart = $this->employeeExpenseChart($employee?$employee->getId():'');
             $crm_expense_chart_detail = $crm_expense_chart && isset( $crm_expense_chart['details']) ? $crm_expense_chart['details']:[];
             unset($crm_expense_chart['details']);
-            $expenseChartArray[] = $crm_expense_chart;
+            $expenseChartArray[] = $crm_expense_chart && sizeof($crm_expense_chart) ? $crm_expense_chart: [];
             $entities['crm_expense_chart'] = $expenseChartArray;
             $entities['crm_expense_chart_detail'] = $crm_expense_chart_detail;
 
