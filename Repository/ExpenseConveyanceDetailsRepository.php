1391d6c9565fcbaf496afefc285e7879c3724850
Rakeybul Hasan <rakeybul.rbs@gmail.com>
2024-03-20 14:01:56 +0600

    expense api modify

--- Repository/ExpenseConveyanceDetailsRepository.php
+++ Repository/ExpenseConveyanceDetailsRepository.php
@@ -73,7 +73,7 @@ class ExpenseConveyanceDetailsRepository extends EntityRepository
     public function getTotalAmountMonthlyByEmployeeYear(User $user, $year=null){
         $qb = $this->createQueryBuilder('e');
         $qb->select('e.id', 'e.transportType', 'SUM(e.totalAmount) as totalAmount');
-        $qb->addSelect('SUM(e.mobilBill) as totalMobileBill','SUM(e.maintenanceBill) as totalMaintenanceBill','SUM(e.tollBill) as totalTollBill','SUM(e.servicingBill) as totalServicingBill','SUM(e.fuelBill) as totalFuelBill', 'SUM(e.parkingBill) as totalParkingBill', 'SUM(e.othersBill) as totalOthersBill', 'SUM(e.amount) as amount');
+        $qb->addSelect('SUM(e.mobilBill) as totalMobileBill','SUM(e.maintenanceBill) as totalMaintenanceBill','SUM(e.tollBill) as totalTollBill','SUM(e.servicingBill) as totalServicingBill','SUM(e.fuelBill) as totalFuelBill', 'SUM(e.parkingBill) as totalParkingBill', 'SUM(e.othersBill) as totalOthersBill', 'SUM(e.amount) as amount', 'SUM(e.totalMileage) as totalMileage');
         $qb->addSelect('expense.id as expenseId');
         $qb->addSelect("DATE_FORMAT(expense.expenseDate,'%b,%y') as expenseWordMonthYear", "DATE_FORMAT(expense.expenseDate,'%Y-%m') as expenseMonthYear", 'YEAR(expense.expenseDate) as expenseYear', 'MONTH(expense.expenseDate) as expenseMonth');
         $qb->addSelect('employee.id as employeeAutoId');
